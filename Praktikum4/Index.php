b522bbd71c1036685b249abf5b795793a99f0416
AlphaSmack <mrtp1988@gmail.com>
2014-09-23 15:02:50 +0300

    'Add sample variables'.

--- Praktikum4/Index.php
+++ Praktikum4/Index.php
@@ -1,7 +1,17 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Administrator
- * Date: 23.09.14
- * Time: 14:55
- */ 
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>harjutus-2 - PHP põhitõed</title>
+</head>
+<body>
+    <h2>Muutujad</h2>
+    <?php
+        $var = 10;
+        echo $var ."<br>";
+
+        $var = 20;
+        echo $var ."<br>"
+    ?>
+</body>
+</html>
