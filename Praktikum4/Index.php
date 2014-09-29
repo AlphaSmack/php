e44770186c7c7ec174be157a6cdd75c56a384735
AlphaSmack <mrtp1988@gmail.com>
2014-09-29 13:37:52 +0300

    'Add sample strings'

--- Praktikum4/Index.php
+++ Praktikum4/Index.php
@@ -13,5 +13,14 @@
         $var = 20;
         echo $var ."<br>"
     ?>
+
+    <h2>Stringid</h2>
+    <?php
+        echo 'Esimene string';
+        echo '<br>';
+        $t=24;
+        $s=26;
+        echo $t,$s;
+    ?>
 </body>
 </html>
