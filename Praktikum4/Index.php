6dc7c07d8115ba12891f5d141f6eb033bae09920
AlphaSmack <mrtp1988@gmail.com>
2014-09-29 13:59:14 +0300

    'Add sample content echo functions'.

--- Praktikum4/Index.php
+++ Praktikum4/Index.php
@@ -22,5 +22,25 @@
         $s=26;
         echo $t,$s;
     ?>
+
+    <h2>Nimekirja väljastamine muutujate ja stringide abil</h2>
+    <?php
+        $student1="Aile";
+        $student2="Tõnis";
+        $student3="Keijo";
+        $student4="Merili";
+        $student5="Maile";
+    ?>
+
+    <ul>
+        <?php
+            echo "<li>".$student1."</li>";
+            echo "<li>".$student2."</li>";
+            echo "<li>".$student3."</li>";
+            echo "<li>".$student4."</li>";
+            echo "<li>".$student5."</li>";
+        ?>
+    </ul>
+
 </body>
 </html>
